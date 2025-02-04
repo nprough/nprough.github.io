4072b101b36146ebf69856a00f999546cbcb2a9a
Nick Prough <nick.prough@gmail.com>
2025-02-04 11:23:51 -0500

    Removed Particles For Test

--- components/Shared/ParticlesBackground.tsx
+++ components/Shared/ParticlesBackground.tsx
@@ -1,6 +1,7 @@
 import Particles, { initParticlesEngine } from "@tsparticles/react";
 import { useEffect, useMemo, useState } from "react";
 import { loadSlim } from "@tsparticles/slim";
+import { ISourceOptions } from "tsparticles-engine"; // importing the ISourceOptions type
 
 const HeroParticles = () => {
   const [, setInit] = useState(false);
@@ -22,76 +23,7 @@ const HeroParticles = () => {
   const particlesLoaded = async () => {
     // console.log(container);
   };
-  const options = useMemo(
-    () => ({
-      background: {
-        color: {
-          value: "transparent",
-        },
-      },
-      fpsLimit: 120,
-      interactivity: {
-        events: {
-          onClick: {
-            enable: false,
-            mode: "push",
-          },
-          onHover: {
-            enable: true,
-            mode: "repulse",
-          },
-        },
-        modes: {
-          push: {
-            quantity: 4,
-          },
-          repulse: {
-            distance: 100,
-            duration: 0.4,
-          },
-        },
-      },
-      particles: {
-        color: {
-          value: "#fb923c",
-        },
-        links: {
-          color: "#ffffff",
-          distance: 150,
-          enable: true,
-          opacity: 0.5,
-          width: 1,
-        },
-        move: {
-          direction: "none",
-          enable: true,
-          outModes: {
-            default: "bounce",
-          },
-          random: false,
-          speed: 2,
-          straight: false,
-        },
-        number: {
-          density: {
-            enable: true,
-          },
-          value: 80,
-        },
-        opacity: {
-          value: 0.5,
-        },
-        shape: {
-          type: "circle",
-        },
-        size: {
-          value: { min: 1, max: 5 },
-        },
-      },
-      fullScreen: { enable: true, zIndex: -1 },
-    }),
-    []
-  );
+  const options = useMemo(() => ({}), []);
 
   return (
     <Particles
