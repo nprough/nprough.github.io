14ebeaf2887aa7791449de36e1af0e6246531755
Nicholas R Prough <nick.prough@gmail.com>
2025-08-08 09:32:22 -0400

    maxx powersports

--- constants/Work.tsx
+++ constants/Work.tsx
@@ -1,6 +1,15 @@
 export const WORKS = [
   {
     id: 1,
+    title: "Maxx Powersports LLC",
+    description:
+      "This website was created for Maxx Powersports LLC.  The website is a modern, clean, and easy to use website that is designed to be used on any device.  I created it using SquareSpace for ease of use for the business staff.  Their website is subject to change as they update their content.",
+    image: "/maxx.png",
+    link: "https://www.maxx-powersports.com/",
+    button: "View Project",
+  },
+  {
+    id: 2,
     title: "Orbisonia United Methodist Church",
     description:
       "This website was created for Orbisonia United Methodist Church.  The website is a simple, clean, and easy to use website that is designed to be used on any device.  I created it using SquareSpace for ease of use for the church staff.  Their website is subject to change as they update their content.",
@@ -9,7 +18,7 @@ export const WORKS = [
     button: "View Project",
   },
   {
-    id: 2,
+    id: 3,
     title: "Woody's Beef Jerky",
     description:
       "This website was created for Woody's Beef Jerky.  Because of this website, Woody's Beef Jerky has been able to expand their reach and sell their jerky all across the United States.  I created this website using SquareSpace for ease of use for the business owner.  The website is subject to change as they update their content.",
@@ -18,7 +27,7 @@ export const WORKS = [
     button: "View Project",
   },
   {
-    id: 3,
+    id: 4,
     title: "Mifflin County Youth Fair",
     description:
       "This website was created for Mifflin County Youth Fair.  Thanks to this website, the Mifflin County Youth fair has been able to expand their reach and get relevant information to their all of their fans.  I created it using SquareSpace for ease of use for the fair staff.  This website has been updated with some new content since it was designed, but overall the design is still the same. The website is still subject to change as they update their content further.",
@@ -27,7 +36,7 @@ export const WORKS = [
     button: "View Project",
   },
   {
-    id: 4,
+    id: 5,
     title: "Our Etsy Store",
     description:
       "Prough Web Design, LLC crafts elegant and user-friendly SquareSpace templates, tailored to elevate your online presence. We also offer a variety of custom designs available for purchase on our Etsy store.",
@@ -36,7 +45,7 @@ export const WORKS = [
     button: "View Store",
   },
   {
-    id: 5,
+    id: 6,
     title: "Github Repositories",
     description:
       "Please check out my Github repositories to see the code for my projects, including this website, which was created using Next.js and Tailwind CSS.  I have a variety of projects that I have worked on, and I am always looking to improve my skills and learn new things.",
--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  /* config options here */
+const nextConfig = {
+  basePath: "/nprough.github.io",
+  output: "export",  // <=== enables static exports
+  reactStrictMode: true,
 };
 
-export default nextConfig;
+module.exports = nextConfig;
